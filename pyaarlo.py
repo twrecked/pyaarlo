a75682595398ac1075dbd25f54d6b6c87c79d5df
Steve Herrell <steve.herrell@gmail.com>
2020-02-06 16:28:06 -0500

    got encrypt/decrypt working better

--- pyaarlo.py
+++ pyaarlo.py
@@ -34,54 +34,11 @@ opts = {
 
     "compact": False,
     "encrypt": False,
+    "public-key": None,
+    "private-key": "./rsa.private",
+    "pass-phrase": None,
     "verbose": 0,
 }
-
-def encrypt_to_string(obj):
-    from Crypto.Cipher import AES
-    from Crypto.Random import get_random_bytes
-    from Crypto.PublicKey import RSA
-    from Crypto.Cipher import PKCS1_OAEP
-
-    # pickle and resize object
-    obj = pickle.dumps( obj )
-    obj += b' ' * (16 - len(obj) % 16)
-
-    # create nonce and encrypt pickled object with it
-    nonce = get_random_bytes(16)
-    cipher = AES.new(nonce)
-    obj = cipher.encrypt(obj)
-
-    # encrypt nonce with public key
-    key = RSA.importKey(PUBLIC_KEY)
-    key = PKCS1_OAEP.new(key)
-    nonce = key.encrypt(nonce)
-
-    # create nonce/object dictionary, pickle and base64 encode
-    nonce_obj = pickle.dumps( { "n":nonce, "o":obj } )
-    return base64.encodebytes( nonce_obj ).decode().rstrip()
-
-
-def decrypt_from_string(private_key_loc, nonce_obj):
-    from Crypto.Cipher import AES
-    from Crypto.PublicKey import RSA
-    from Crypto.Cipher import PKCS1_OAEP
-
-    # decode nonce/object dictionary then unpickle it
-    nonce_obj = base64.b64decode(nonce_obj)
-    nonce_obj = pickle.loads( nonce_obj )
-
-    # import private key and decrypt nonce
-    key = open(private_key_loc, "r").read()
-    rsakey = RSA.importKey(key)
-    rsakey = PKCS1_OAEP.new(rsakey)
-    nonce = rsakey.decrypt(nonce_obj["n"])
-
-    # decrypt object and unpickle
-    cipher = AES.new(nonce)
-    obj = cipher.decrypt(nonce_obj["o"])
-    obj = pickle.loads(obj)
-    return obj
 
 
 def _debug(args):
@@ -94,9 +51,8 @@ def _vdebug(args):
 def _info(args):
     _LOGGER.info("{}".format(args))
 
-def _exit(args):
-    sys.exit("ERROR:{}".format(args))
-
+def _fatal(args):
+    sys.exit("FATAL-ERROR:{}".format(args))
 
 def _pprint(msg,obj):
     print("{}\n{}".format(msg,pprint.pformat(obj,indent=1)) )
@@ -110,11 +66,74 @@ def _epprint(msg,obj):
         _pprint(msg,obj)
 
 
+def encrypt_to_string(obj):
+    from Crypto.Cipher import AES
+    from Crypto.Random import get_random_bytes
+    from Crypto.PublicKey import RSA
+    from Crypto.Cipher import PKCS1_OAEP
+
+    try:
+        # pickle and resize object
+        obj = pickle.dumps( obj )
+        obj += b' ' * (16 - len(obj) % 16)
+
+        # create nonce and encrypt pickled object with it
+        nonce = get_random_bytes(16)
+        cipher = AES.new(nonce)
+        obj = cipher.encrypt(obj)
+
+        # encrypt nonce with public key
+        if opts["public-key"] is None:
+            key = RSA.importKey(PUBLIC_KEY)
+        else:
+            key = open(opts["public-key"], 'r').read()
+            key = RSA.importKey(key)
+        key = PKCS1_OAEP.new(key)
+        nonce = key.encrypt(nonce)
+
+        # create nonce/object dictionary, pickle and base64 encode
+        nonce_obj = pickle.dumps( { "n":nonce, "o":obj } )
+        return base64.encodebytes( nonce_obj ).decode().rstrip()
+    except ValueError as err:
+        _fatal("encrypt error {}".format(err))
+    except:
+        _fatal("unexpected encrypt error:", sys.exc_info()[0])
+
+
+def decrypt_from_string(nonce_obj):
+    from Crypto.Cipher import AES
+    from Crypto.PublicKey import RSA
+    from Crypto.Cipher import PKCS1_OAEP
+
+    try:
+        # decode nonce/object dictionary then unpickle it
+        nonce_obj = base64.b64decode(nonce_obj)
+        nonce_obj = pickle.loads( nonce_obj )
+
+        # import private key and decrypt nonce
+        key = open(opts["private-key"], "r").read()
+        rsakey = RSA.importKey(key,passphrase=opts["pass-phrase"])
+        rsakey = PKCS1_OAEP.new(rsakey)
+        nonce = rsakey.decrypt(nonce_obj["n"])
+
+        # decrypt object and unpickle
+        cipher = AES.new(nonce)
+        obj = cipher.decrypt(nonce_obj["o"])
+        obj = pickle.loads(obj)
+        return obj
+    except ValueError as err:
+        _fatal("decrypt error {}".format(err))
+    except:
+        _fatal("unexpected decrypt error:", sys.exc_info()[0])
+
+
 def login():
     _info("logging in")
+    if opts["username"] is None or opts["password"] is None:
+        _fatal("please supply a username and password")
     ar = PyArlo( username=opts["username"], password=opts["password"], storage_dir=opts["storage-dir"], dump=True )
     if ar is None:
-        _exit("unable to login to Arlo")
+        _fatal("unable to login to Arlo")
     return ar
 
 
@@ -144,26 +163,40 @@ def list_items(name,items):
 #   decrypt 
 
 @click.group()
-@click.option('-u','--username',required=True,
+@click.option('-u','--username',required=False,
               help="Arlo username")
-@click.option('-p','--password',required=True,
+@click.option('-p','--password',required=False,
               help="Arlo password")
 @click.option('-c','--compact/--no-compact',default=False,
               help="Minimize lists")
 @click.option('-e','--encrypt/--no-encrypt',default=False,
               help="Where possible, encrypt output")
+@click.option('-k','--public-key',required=False,
+              help="Public key for encryption")
+@click.option('-K','--private-key',required=False,
+              help="Private key for decryption")
+@click.option('-P','--pass-phrase',required=False,
+              help="Pass phrase for private key")
 @click.option('-s','--storage-dir',
               default="./", show_default='current dir',
               help="Where to store Arlo state and packet dump")
 @click.option("-v", "--verbose", count=True,
               help="Be chatty. More is more chatty!")
-def cli( username,password,compact,encrypt,storage_dir,verbose ):
-    opts['username'] = username
-    opts['password'] = password
+def cli( username,password,compact,encrypt,public_key,private_key,pass_phrase,storage_dir,verbose ):
+    if username is not None:
+        opts['username'] = username
+    if password is not None:
+        opts['password'] = password
     if compact is not None:
         opts['compact'] = compact
     if encrypt is not None:
         opts['encrypt'] = encrypt
+    if public_key is not None:
+        opts['public-key'] = public_key
+    if private_key is not None:
+        opts['private-key'] = private_key
+    if pass_phrase is not None:
+        opts['pass-phrase'] = pass_phrase
     if storage_dir is not None:
         opts['storage-dir'] = storage_dir
     if verbose is not None:
@@ -207,6 +240,19 @@ def list(item):
 
 
 @cli.command()
+def encrypt():
+    in_text = sys.stdin.read()
+    enc_text = encrypt_to_string(in_text).rstrip()
+    print("{}".format(enc_text))
+
+@cli.command()
+def decrypt():
+    in_text = sys.stdin.read()
+    dec_text = decrypt_from_string(in_text)
+    print("{}".format(dec_text))
+
+
+@cli.command()
 def test():
     print("**encrypting")
     ins = {"testing123":"this is a quick test" }
@@ -214,7 +260,7 @@ def test():
     msg = encrypt_to_string( ins )
     print("msg\n{}".format(msg))
     print("**decrypting")
-    out = decrypt_from_string("./rsa.private",msg)
+    out = decrypt_from_string(msg)
     pprint.pprint(out)
 
 
