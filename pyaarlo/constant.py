4e928677a337cfaab5b40848541780ba852e4fe8
Steve Herrell <steve.herrell@gmail.com>
2020-03-24 00:21:05 -0400

    added imap based 2fa support

--- pyaarlo/constant.py
+++ pyaarlo/constant.py
@@ -27,6 +27,10 @@ AUTH_VALIDATE_PATH = '/api/validateAccessToken'
 TFA_HOST = "https://pyaarlo-tfa.appspot.com/"
 TFA_CODE_PATH = "/get"
 TFA_CLEAR_PATH = "/clear"
+TFA_CONSOLE_SOURCE = 'console'
+TFA_IMAP_SOURCE = 'imap'
+TFA_EMAIL_TYPE = 'EMAIL'
+TFA_SMS_TYPE = 'SMS'
 
 PRELOAD_DAYS = 30
 
