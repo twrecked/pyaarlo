634df129263c081a4beca7dc7bd9d646ad9e1ab9
Michael Urspringer <michael@urspringer.de>
2021-10-24 11:05:39 +0200

    Activated debug mode to get more inforation about this probem

--- arlo-fhem.py
+++ arlo-fhem.py
@@ -5,7 +5,7 @@
 # Based on https://github.com/twrecked/pyaarlo
 # Michael Urspringer
 
-VERSION = "1.1.8a"
+VERSION = "1.1.8b"
 
 import pyaarlo
 import argparse
@@ -29,7 +29,7 @@ def loginToArlo(username, password, tfa_host, tfa_username, tfa_password, max_tr
     while count < max_tries:
         count = count + 1 
         print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "- arlo-fhem - Trying to connect ",count," of ",max_tries)
-        arlo = pyaarlo.PyArlo(username=username, password=password,tfa_source='imap', tfa_type='email', tfa_host=tfa_host, tfa_username=tfa_username, tfa_password=tfa_password, synchronous_mode=False, refresh_devices_every=1,reconnect_every=60, stream_timeout=180, request_timeout=120, verbose_debug=False)
+        arlo = pyaarlo.PyArlo(username=username, password=password,tfa_source='imap', tfa_type='email', tfa_host=tfa_host, tfa_username=tfa_username, tfa_password=tfa_password, synchronous_mode=False, refresh_devices_every=1,reconnect_every=60, stream_timeout=180, request_timeout=120, verbose_debug=True)
         if arlo.is_connected:
             break
         if count == max_tries:
@@ -59,7 +59,7 @@ def getDeviceFromName(name, devices):
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "- arlo-fhem - version", VERSION)
 
 # set up logging, change ERROR or INFO to DEBUG for a *lot* more information
-logging.basicConfig(level=logging.ERROR,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     filename='debug.log')
 _LOGGER = logging.getLogger('arlo-fhem')
