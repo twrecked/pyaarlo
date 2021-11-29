da0310127f1cdc7ffedba371016b26af18fbf0c4
Steve Herrell <steve.herrell@gmail.com>
2021-11-28 22:00:03 -0500

    Get correct initial values from new devices.
    Newer devices don't support the "getting resource" so pass the information
    in as part of the device request.

--- pyaarlo/__init__.py
+++ pyaarlo/__init__.py
@@ -40,7 +40,7 @@ from .util import time_to_arlotime
 
 _LOGGER = logging.getLogger("pyaarlo")
 
-__version__ = "0.8.0a12"
+__version__ = "0.8.0a13"
 
 
 class PyArlo(object):
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 setup(
 
     name='pyaarlo',
-    version='0.8.0a12',
+    version='0.8.0a13',
     packages=['pyaarlo'],
 
     python_requires='>=3.6',
