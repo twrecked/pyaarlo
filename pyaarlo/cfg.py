5fc4af638e383d8aa7e5e01a301365b7980ffb42
Steve Herrell <steve.herrell@gmail.com>
2021-12-13 23:32:18 -0500

    Don't get resources for

--- pyaarlo/cfg.py
+++ pyaarlo/cfg.py
@@ -196,7 +196,6 @@ class ArloCfg(object):
     @property
     def session_file(self):
         return self.storage_dir + "/session.pickle"
-        return None
 
     @property
     def save_session(self):
@@ -239,3 +238,7 @@ class ArloCfg(object):
     @property
     def no_unicode_squash(self):
         return self._kw.get("no_unicode_squash", True)
+
+    @property
+    def use_mqtt(self):
+        return self._kw.get("backend", "mqtt") == "mqtt"
