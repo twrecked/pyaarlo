2fc191673fc2b44aaf7e44c7fa40a39eb1e545f8
Steve Herrell <steve.herrell@gmail.com>
2022-10-18 22:54:54 -0400

    Better clean up after a failed snapshot.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 setup(
 
     name='pyaarlo',
-    version='0.8.0b2',
+    version='0.8.0b3',
     packages=['pyaarlo'],
 
     python_requires='>=3.6',
@@ -20,7 +20,7 @@ setup(
         'click',
         'pycryptodome',
         'unidecode',
-        'cloudscraper>=1.2.58',
+        'cloudscraper>=1.2.64',
         'paho-mqtt',
         'cryptography'
     ],
