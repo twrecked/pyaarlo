226ffc763951decbb9143c9d1d689b654dc6befa
Steve Herrell <steve.herrell@gmail.com>
2024-09-04 09:49:48 -0400

    Bumped revision...

--- pyaarlo/__init__.py
+++ pyaarlo/__init__.py
@@ -52,7 +52,7 @@ from .util import time_to_arlotime
 
 _LOGGER = logging.getLogger("pyaarlo")
 
-__version__ = "0.8.0.9"
+__version__ = "0.8.0.10"
 
 
 class PyArlo(object):
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 setup(
 
     name='pyaarlo',
-    version='0.8.0.9',
+    version='0.8.0.10',
     packages=['pyaarlo'],
 
     python_requires='>=3.7',
